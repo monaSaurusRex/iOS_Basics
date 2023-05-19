aaf33adb27bad7ec2e2b4b106c6f2ea0715c5d50
Monique <monqiquenagel24.mn@gmail.com>
2023-05-19 12:09:23 +0200

    Added current iOS basic project files.

---
+++ CocoTouch/CocoTouch/MyButton.swift
@@ -0,0 +1,20 @@
+//
+//  MyButton.swift
+//  CocoTouch
+//
+//  Created by DA MAC M1 158 on 2023/05/19.
+//
+
+import UIKit
+
+class MyButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ I_Am_RIch/I_Am_RIch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I_Am_RIch
+//
+//  Created by DA MAC M1 158 on 2023/05/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
