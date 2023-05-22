eb511c0d313ed901cd7ba54a52da28a1ce0fdf5a
Monique <monqiquenagel24.mn@gmail.com>
2023-05-22 12:16:51 +0200

    Made dice app randomly change numbers when button is clicked.

--- DiceAppAssessment/DiceAppAssessment/ViewController.swift
+++ DiceAppAssessment/DiceAppAssessment/ViewController.swift
@@ -12,15 +12,26 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImage1:UIImageView!
     @IBOutlet weak var diceImage2: UIImageView!
     
+    var leftDiceNumber = 5
+    var rightDiceNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func RollDiceClicked(_ sender: Any) {
-        //who.what = value
-        diceImage1.image = UIImage(named: "5")
-        diceImage2.image = #imageLiteral(resourceName: "3")
+        
+        //who.what = valu
+        /*------TRAVERSE LINEAR------*/
+        var diceSides = [#imageLiteral(resourceName: "Dice_One"), #imageLiteral(resourceName: "2"), #imageLiteral(resourceName: "3"), #imageLiteral(resourceName: "4"), #imageLiteral(resourceName: "5"), #imageLiteral(resourceName: "6")]
+        
+        diceImage1.image = diceSides[Int.random(in: 0...5)]
+        
+//        diceImage2.image = diceSides[Int.random(in: 0...6)]
+        
+        /*------TRAVERSE RANDOMLY------*/
+        
     }
 }
 
